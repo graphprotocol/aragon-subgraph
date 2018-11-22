3dfecfe651f509deeb7ce85588bdb708415f6557
David Kajpust <kajpustd@gmail.com>
2018-11-22 14:07:59 -0500

    all contracts I am going to source are sourced. still a few bugs to kink out

---
+++ aragon-network-subgraph/mappings/ENSResolverFIFS.ts
@@ -0,0 +1,29 @@
+import 'allocator/arena'
+export { allocate_memory }
+
+// Import APIs from graph-ts
+import { store } from '@graphprotocol/graph-ts'
+
+// Import event types from the registrar contract ABI
+import {ClaimSubdomain} from '../types/ENSResolver/ENSResolver'
+
+// Import entity types from the schema
+import {ENSResolver} from "../types/schema";
+
+
+//This mapping is named FIFSResolvingRegistrar on etherscan. Code is found under aragon-id repo
+// mainnet - https://etherscan.io/address/0x546aa2eae2514494eeadb7bbb35243348983c59d#events
+// testnet - https://rinkeby.etherscan.io/address/0x3665e7bfd4d3254ae7796779800f5b603c43c60d#events
+
+export function handleClaimSubdomain(event: ClaimSubdomain): void {
+  let id = event.params.subnode.toHex()
+  let owner = event.params.owner
+  let resolver = event.params.resolver
+
+  let subdomain = new ENSResolver()
+  subdomain.owner = owner
+  subdomain.resolver = resolver
+
+  store.set("ENSResolver", id, subdomain)
+}
+
